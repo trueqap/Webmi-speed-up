61fa3e7007e2d171263a229d3af8f77986ed2a2e
trueqap <trueqap@gmail.com>
2018-08-25 19:27:46 +0200

    0.0.7

--- init.php
+++ init.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * Plugin Name:       Web:Mi Speed Up 
+ * Plugin Name:       Web:Mi Speed Up
  * Plugin URI:        https://github.com/trueqap/Webmi-speed-up
  * Description:       WordPress Speed Up package
- * Version:           0.0.6
+ * Version:           0.0.7
  * Author:            Laszlo Patai
  * Author URI:        https://webmi.io
  * Text Domain:       webmi-speed-up
@@ -11,26 +11,26 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
  */
- 
-if ( ! defined( 'WPINC' ) ) {
+
+if (!defined('WPINC')) {
     die;
 }
- 
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-loader.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/updater/plugin-update-checker.php';
 
+require_once plugin_dir_path(__FILE__) . 'includes/class-plugin-loader.php';
+require_once plugin_dir_path(__FILE__) . 'includes/updater/plugin-update-checker.php';
 
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://github.com/trueqap/WebMi-Speed-Up/',
-	__FILE__,
-	'webmi-speed-up'
+    'https://github.com/trueqap/WebMi-Speed-Up/',
+    __FILE__,
+    'webmi-speed-up'
 );
- 
-function run_plugin_loader() {
- 
+
+function run_plugin_loader()
+{
+
     $wsu = new WebMi_Speed_Up();
-   	return $wsu;
- 
+    return $wsu;
+
 }
- 
+
 run_plugin_loader();
